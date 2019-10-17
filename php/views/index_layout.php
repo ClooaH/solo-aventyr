730559e4a98b484b8f08dfc1d73b63e64f96bc28
ClooaH <a.s.d.w.a.s@hotmail.com>
2019-10-17 11:01:10 +0200

    fixade lite grejer som ett template för ljud och vet hur man boxar in text enligt min skiss.

--- php/views/index_layout.php
+++ php/views/index_layout.php
@@ -5,21 +5,35 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title><?= $pageTitle; ?></title>
+    <script>
+    var sound = new Audio("sound/LuigiNutting.mp3");
+    </script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
     <main>
-        <p>
-            <?= $story['body'] ?>
-        </p>
-
+        <nav class="navbar navbar-dark bg-dark">
+            <a class="navbar-brand" href="?id=1" onClick="sound.play()">
+            <img src="img/Luigisupernut.png" width="30" height="30" class="d-inline-block align-top" alt="You cannot see the great picture. :C">
+            Luigi Super Nut
+            </a>
+        </nav>
+        <div class="w3-container coolguy">
+            <p>
+                <?= $story['body'] ?>
+            </p>
+        </div>
         <ul>
             <?php foreach ($links as $link): ?>
-            <a href="index.php?id=<?= $link['target_id'] ?>">
+            <a id="target" href="index.php?id=<?= $link['target_id'] ?>">
                 <li><?= $link['description'] ?></li>
             </a>
             <?php endforeach ?>
         </ul>
     </main>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
 </html>
